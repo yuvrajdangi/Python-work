3df901033f2c18dbecf3ca0c14d0f1c117792721
yuvrajdangi <134692739+yuvrajdangi@users.noreply.github.com>
2023-06-01 06:39:21 +0530

    Add files via upload

---
+++ pallindrome no or not.py
@@ -0,0 +1,7 @@
+num =input("enter any number")
+num2=num[::-1]
+print(num2)
+if (num==num2):
+  print("number is pallindrome")
+else:
+  print("number is not pallindrome" )
---
+++ prime no.or not.py
@@ -0,0 +1,13 @@
+num = 29
+flag = false
+if num == 1:
+   print(num,"is not a prime number")
+elif num > 1:
+     for i in range(2,num):
+        if(num%1)==0:
+            flag == true
+                 break:
+if flag:
+   print(num, "is not a prime number")
+else:
+   print(num,"is a prime number")
